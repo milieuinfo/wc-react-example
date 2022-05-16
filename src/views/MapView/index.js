8946e448652e6768bd192e79bd6da01f6b58590c
Lucas Sledsens <lucas_sledsens@hotmail.com>
2022-05-16 17:59:27 +0200

    openlayers example

--- src/views/MapView/index.js
+++ src/views/MapView/index.js
@@ -1,124 +1,106 @@
-import React, { useEffect, useRef } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState } from "react";
 import Page from "../../components/Page";
 import SectionWrap from "../../components/SectionWrap";
 import "uig-webcomponents/lib/components/map";
-import FeaturesLayer from "./FeaturesLayer";
+import Map from "ol/Map";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import View from "ol/View";
+import "ol/ol.css";
+import TileLayer from "ol/layer/Tile";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import XYZ from "ol/source/XYZ";
+import { Circle, Fill, Style, Text } from "ol/style";
 import Select from "ol/interaction/Select";
-import OlStyle from "ol/style/Style";
-import OlStyleStroke from "ol/style/Stroke";
-import OlStyleFill from "ol/style/Fill";
-import {
-  altKeyOnly,
-  click,
-  pointerMove,
-  singleClick,
-} from "ol/events/condition";
-import OlStyleCircle from "ol/style/Circle";
-import OlStyleText from "ol/style/Text";
-
-const types = {
-  o: {
-    id: 1,
-    style: new OlStyle({
-      image: new OlStyleCircle({
-        fill: new OlStyleFill({
-          color: "#003B8E",
-        }),
-        radius: 11,
-      }),
-      text: new OlStyleText({
-        font: "bold 14px Flanders Art Sans,sans-serif",
-        fill: new OlStyleFill({ color: "#fff" }),
-        text: "O",
-      }),
-    }),
-  },
-  b: {
-    id: 2,
-    style: new OlStyle({
-      image: new OlStyleCircle({
-        fill: new OlStyleFill({
-          color: "#AD3E00",
-        }),
-        radius: 11,
-      }),
-      text: new OlStyleText({
-        font: "bold 14px Flanders Art Sans,sans-serif",
-        fill: new OlStyleFill({ color: "#fff" }),
-        text: "B",
-      }),
-    }),
-  },
-};
+import { pointerMove } from "ol/events/condition";
 
 const MapView = () => {
   const mapRef = useRef();
-  const layerRef = useRef();
-  const selectActionRef = useRef();
+  const [map, setMap] = useState();
+  const [features, setFeatures] = useState([
+    new Feature({
+      geometry: new Point([210000, 190000]),
+    }),
+  ]);
+  const [featuresLayer, setFeaturesLayer] = useState(
+    new VectorLayer({
+      source: new VectorSource(),
+      style: new Style({
+        image: new Circle({
+          fill: new Fill({
+            color: "#AD3E00",
+          }),
+          radius: 11,
+        }),
+        text: new Text({
+          font: "bold 14px Flanders Art Sans,sans-serif",
+          fill: new Fill({ color: "#fff" }),
+          text: "B",
+        }),
+      }),
+    })
+  );
+
+  const selected = new Style({
+    image: new Circle({
+      fill: new Fill({
+        color: "#AD3E00",
+      }),
+      radius: 11,
+    }),
+    text: new Text({
+      font: "bold 14px Flanders Art Sans,sans-serif",
+      fill: new Fill({ color: "#fff" }),
+      text: "Selected!",
+    }),
+  });
 
   useEffect(() => {
-    layerRef.current.layer
-      .getSource()
-      .getFeatures()
-      .forEach((feature) => {
-        const { typeId } = feature.getProperties();
-        feature.setStyle(typeId === types.o.id ? types.o.style : types.b.style);
-      });
-  }, [layerRef]);
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
+          }),
+        }),
+        featuresLayer,
+      ],
+      target: mapRef.current,
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
 
-  const geojsonObject = {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        properties: {
-          typeId: types.o.id,
-        },
-        geometry: {
-          type: "Point",
-          coordinates: [149055.0, 199908.0],
-        },
-      },
-      {
-        type: "Feature",
-        properties: {
-          typeId: types.b.id,
-        },
-        geometry: {
-          type: "Point",
-          coordinates: [151055.0, 201908.0],
-        },
-      },
-    ],
-  };
+    map.addInteraction(new Select({ style: () => selected }));
+    map.addInteraction(
+      new Select({ style: () => selected, condition: pointerMove })
+    );
 
-  // useEffect(() => {
-  //   selectActionRef.current.style = new OlStyle({
-  //     image: new OlStyleCircle({
-  //       fill: new OlStyleFill({
-  //         color: "#AD3E00",
-  //       }),
-  //       radius: 11,
-  //     }),
-  //     text: new OlStyleText({
-  //       font: "bold 14px Flanders Art Sans,sans-serif",
-  //       fill: new OlStyleFill({ color: "#fff" }),
-  //       text: "Selected",
-  //     }),
-  //   });
-  // }, [selectActionRef]);
+    setMap(map);
+  }, []);
+
+  useEffect(() => {
+    if (features.length > 0 && map) {
+      featuresLayer.setSource(
+        new VectorSource({
+          features: features,
+        })
+      );
+
+      // map.getView().fit(featuresLayer.getSource().getExtent(), {
+      //   padding: [100, 100, 100, 100],
+      // });
+    }
+  }, [features, map]);
 
   return (
     <Page title="Map">
       <SectionWrap>
         <div is="vl-column" data-vl-size="12">
-          <vl-map ref={mapRef}>
-            <vl-map-baselayer-grb-ortho></vl-map-baselayer-grb-ortho>
-            <FeaturesLayer
-              ref={layerRef}
-              features={geojsonObject}
-            ></FeaturesLayer>
-          </vl-map>
+          <div style={{ width: "100%", height: "500px" }} ref={mapRef}></div>
         </div>
       </SectionWrap>
     </Page>
---
+++ src/views/MapView/old.js
@@ -0,0 +1,128 @@
+// import React, { useEffect, useRef } from "react";
+// import Page from "../../components/Page";
+// import SectionWrap from "../../components/SectionWrap";
+// import "uig-webcomponents/lib/components/map";
+// import FeaturesLayer from "./FeaturesLayer";
+// import Select from "ol/interaction/Select";
+// import OlStyle from "ol/style/Style";
+// import OlStyleStroke from "ol/style/Stroke";
+// import OlStyleFill from "ol/style/Fill";
+// import {
+//   altKeyOnly,
+//   click,
+//   pointerMove,
+//   singleClick,
+// } from "ol/events/condition";
+// import OlStyleCircle from "ol/style/Circle";
+// import OlStyleText from "ol/style/Text";
+
+// const types = {
+//   o: {
+//     id: 1,
+//     style: new OlStyle({
+//       image: new OlStyleCircle({
+//         fill: new OlStyleFill({
+//           color: "#003B8E",
+//         }),
+//         radius: 11,
+//       }),
+//       text: new OlStyleText({
+//         font: "bold 14px Flanders Art Sans,sans-serif",
+//         fill: new OlStyleFill({ color: "#fff" }),
+//         text: "O",
+//       }),
+//     }),
+//   },
+//   b: {
+//     id: 2,
+//     style: new OlStyle({
+//       image: new OlStyleCircle({
+//         fill: new OlStyleFill({
+//           color: "#AD3E00",
+//         }),
+//         radius: 11,
+//       }),
+//       text: new OlStyleText({
+//         font: "bold 14px Flanders Art Sans,sans-serif",
+//         fill: new OlStyleFill({ color: "#fff" }),
+//         text: "B",
+//       }),
+//     }),
+//   },
+// };
+
+// const MapView = () => {
+//   const mapRef = useRef();
+//   const layerRef = useRef();
+//   const selectActionRef = useRef();
+
+//   useEffect(() => {
+//     layerRef.current.layer
+//       .getSource()
+//       .getFeatures()
+//       .forEach((feature) => {
+//         const { typeId } = feature.getProperties();
+//         feature.setStyle(typeId === types.o.id ? types.o.style : types.b.style);
+//       });
+//   }, [layerRef]);
+
+//   const geojsonObject = {
+//     type: "FeatureCollection",
+//     features: [
+//       {
+//         type: "Feature",
+//         properties: {
+//           typeId: types.o.id,
+//         },
+//         geometry: {
+//           type: "Point",
+//           coordinates: [149055.0, 199908.0],
+//         },
+//       },
+//       {
+//         type: "Feature",
+//         properties: {
+//           typeId: types.b.id,
+//         },
+//         geometry: {
+//           type: "Point",
+//           coordinates: [151055.0, 201908.0],
+//         },
+//       },
+//     ],
+//   };
+
+//   // useEffect(() => {
+//   //   selectActionRef.current.style = new OlStyle({
+//   //     image: new OlStyleCircle({
+//   //       fill: new OlStyleFill({
+//   //         color: "#AD3E00",
+//   //       }),
+//   //       radius: 11,
+//   //     }),
+//   //     text: new OlStyleText({
+//   //       font: "bold 14px Flanders Art Sans,sans-serif",
+//   //       fill: new OlStyleFill({ color: "#fff" }),
+//   //       text: "Selected",
+//   //     }),
+//   //   });
+//   // }, [selectActionRef]);
+
+//   return (
+//     <Page title="Map">
+//       <SectionWrap>
+//         <div is="vl-column" data-vl-size="12">
+//           <vl-map ref={mapRef}>
+//             <vl-map-baselayer-grb-ortho></vl-map-baselayer-grb-ortho>
+//             <FeaturesLayer
+//               ref={layerRef}
+//               features={geojsonObject}
+//             ></FeaturesLayer>
+//           </vl-map>
+//         </div>
+//       </SectionWrap>
+//     </Page>
+//   );
+// };
+
+// export default MapView;
